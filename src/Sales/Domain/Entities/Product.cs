d1d58c3960d6ae11ff15f84713eed11814d80582
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2023-07-30 19:07:37 -0300

    Add Product entity in Sales domain
    In our Sales domain, a new entity called 'Product' is added. This was necessary to properly represent and handle 'Product' related data and operations. It currently includes only a name property.

---
+++ src/Sales/Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Daiarts.Sales.Domain.Entities;
+
+public class Product : Entity
+{
+    public Product(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+}
