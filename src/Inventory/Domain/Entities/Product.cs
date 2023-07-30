8258c4b2120b1edabe262ac4f8625117ee137dc3
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2023-07-30 18:46:28 -0300

    Add new domain entities and value objects to Inventory
    This commit introduces several new classes in the Inventory domain. These include the FeedstockProduct and Product entities as well as the StockKeepingUnit value object. These are added to better model the domain, improve separation of concerns, ensure domain logic consistency and enhance code readability. It also includes UnitOfMeasurement enum for easier handling of product measures.

---
+++ src/Inventory/Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Daiarts.Inventory.Domain.Entities;
+
+public sealed class Product : Entity
+{
+    public Product(string name, StockKeepingUnit sku, int manufacturingTime)
+    {
+        Name = name;
+        Sku = sku;
+        ManufacturingTime = manufacturingTime;
+    }
+
+    public string Name { get; private set; }
+    public StockKeepingUnit Sku { get; private set; }
+    public int ManufacturingTime { get; private set; }
+}
