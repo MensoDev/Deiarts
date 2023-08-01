b77e2338c15db7c7852e76cccc255943a25950d4
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2023-08-01 20:28:46 -0300

    ✨ Progride Pricing Context

---
+++ src/Pricing/Domain/Entities/Budget.cs
@@ -0,0 +1,13 @@
+﻿namespace Daiarts.Pricing.Domain.Entities;
+
+public class Budget : Entity
+{
+    private readonly List<BudgetItem> _items = new();
+    
+    public IReadOnlyCollection<BudgetItem> Items => _items.AsReadOnly();
+    
+    public void AddItem(Guid productId, decimal quantity)
+    {
+        _items.Add(new BudgetItem(Id, productId, quantity));
+    }
+}
---
+++ src/Pricing/Domain/Usings.cs
@@ -0,0 +1,1 @@
+﻿global using Daiarts.Common.Domain.Entities;
