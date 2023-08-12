a8aa6414bb48fdfe188dae82fe396c1f806449be
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2023-08-12 20:44:40 -0300

    ✅ | Write tests for FeedstockProduct | closes #3

---
+++ tests/Prototype/Tests/Entities/FeedstockProductTests.cs
@@ -0,0 +1,75 @@
+﻿namespace Daiarts.Prototype.Tests.Entities;
+
+public class FeedstockProductTests
+{
+    [Fact]
+    public void ShouldReturnErrorWhenFeedstockIdIsEmpty()
+    {
+        // Arrange
+        var feedstockId = Guid.Empty;
+        var productId = Guid.NewGuid();
+        const UnitOfMeasurement unitOfMeasurement = UnitOfMeasurement.Kilogram;
+        const decimal quantity = 1m;
+
+        // Act
+        var feedstockProduct = new FeedstockProduct(feedstockId, productId, unitOfMeasurement, quantity);
+
+        // Assert
+        feedstockProduct.IsValid.Should().BeFalse();
+        feedstockProduct.Notifications.Should().Contain(notification => notification.Key == nameof(feedstockProduct.FeedstockId));
+        feedstockProduct.Notifications.Should().Contain(notification => notification.Message == "Feedstock is required");
+    }
+    
+    [Fact]
+    public void ShouldReturnErrorWhenProductIdIsEmpty()
+    {
+        // Arrange
+        var feedstockId = Guid.NewGuid();
+        var productId = Guid.Empty;
+        const UnitOfMeasurement unitOfMeasurement = UnitOfMeasurement.Kilogram;
+        const decimal quantity = 1m;
+
+        // Act
+        var feedstockProduct = new FeedstockProduct(feedstockId, productId, unitOfMeasurement, quantity);
+
+        // Assert
+        feedstockProduct.IsValid.Should().BeFalse();
+        feedstockProduct.Notifications.Should().Contain(notification => notification.Key == nameof(feedstockProduct.ProductId));
+        feedstockProduct.Notifications.Should().Contain(notification => notification.Message == "Product is required");
+    }
+    
+    [Fact]
+    public void ShouldReturnErrorWhenQuantityIsLessThanZero()
+    {
+        // Arrange
+        var feedstockId = Guid.NewGuid();
+        var productId = Guid.NewGuid();
+        const UnitOfMeasurement unitOfMeasurement = UnitOfMeasurement.Kilogram;
+        const decimal quantity = -1m;
+
+        // Act
+        var feedstockProduct = new FeedstockProduct(feedstockId, productId, unitOfMeasurement, quantity);
+
+        // Assert
+        feedstockProduct.IsValid.Should().BeFalse();
+        feedstockProduct.Notifications.Should().Contain(notification => notification.Key == nameof(feedstockProduct.Quantity));
+        feedstockProduct.Notifications.Should().Contain(notification => notification.Message == "Quantity must be greater than 0");
+    }
+    
+    [Fact]
+    public void ShouldReturnSuccessWhenFeedstockProductIsValid()
+    {
+        // Arrange
+        var feedstockId = Guid.NewGuid();
+        var productId = Guid.NewGuid();
+        const UnitOfMeasurement unitOfMeasurement = UnitOfMeasurement.Kilogram;
+        const decimal quantity = 1m;
+
+        // Act
+        var feedstockProduct = new FeedstockProduct(feedstockId, productId, unitOfMeasurement, quantity);
+
+        // Assert
+        feedstockProduct.IsValid.Should().BeTrue();
+        feedstockProduct.Notifications.Should().BeEmpty();
+    }
+}
